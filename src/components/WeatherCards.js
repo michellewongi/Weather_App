ea31c1e789cccc9aa42b86efac2f2886f029c0ca
mw3981 <mw3981@nyu.edu>
2021-09-30 10:05:02 -0400

    Updating css file

--- src/components/WeatherCards.js
+++ src/components/WeatherCards.js
@@ -4,7 +4,7 @@ function WeatherCard({
   cloudiness,
   currentTemp,
   highTemp,
-  humiditiy,
+  humidity,
   lowTemp,
   weatherType = "Unknown",
   windSpeed,
@@ -12,26 +12,28 @@ function WeatherCard({
   return (
     <section className="WeatherCard">
       <p className="currentTemp">
-        Current Temp: <strong>{currentTemp}</strong>
+        <strong>{currentTemp}°</strong>
       </p>
-      <p>
-        Weather Type: <strong>{weatherType}</strong>
-      </p>
-      <p>
-        Cloudiness: <strong>{cloudiness}</strong>
-      </p>
-      <p>
-        High Temp: <strong>{highTemp}</strong>
-      </p>
-      <p>
-        Humiditiy: <strong>{humiditiy}</strong>
-      </p>
-      <p>
-        Low Temp: <strong>{lowTemp}</strong>
-      </p>
-      <p>
-        Wind Speed: <strong>{windSpeed}</strong>
-      </p>
+      <div className="other-info">
+        <p className="weather-type">
+          Weather Type: <strong> {weatherType}</strong>
+        </p>
+        <p className="cloudiness">
+          Cloudiness: <strong> {cloudiness}</strong>
+        </p>
+        <p className="high">
+          High: <strong> {highTemp}</strong>
+        </p>
+        <p className="humidity">
+          Humidity: <strong> {humidity}</strong>
+        </p>
+        <p className="low">
+          Low: <strong> {lowTemp}</strong>
+        </p>
+        <p className="wind">
+          Wind Speed: <strong> {windSpeed}</strong>
+        </p>
+      </div>
     </section>
   );
 }
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,37 +1,90 @@
-import React, { useMemo } from "react";
+import React, { useEffect, useMemo, useState } from "react";
+//axios allows you to make HTTP requests
+import axios from "axios";
+import { useLocation } from "react-router";
 import WeatherCard from "../components/WeatherCards";
 
 const APIKey = "8fb50a254d1d00c642f7408ac5294de5";
 
+// URL search parameters
+// localhost:3000/?city=paris
+// Abstract away URL Search Params here to make it easier to use
+function useQuery() {
+  return new URLSearchParams(useLocation().search);
+}
+
+function fahrenheit(kelvin) {
+  let newTemp = Math.floor((kelvin - 273) * (9 / 5) + 32);
+  return newTemp;
+}
+
 function Home() {
+  const [city, setCity] = useState();
+  const [weatherData, setWeatherData] = useState();
+  let query = useQuery();
+
+  const URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`;
+
+  useEffect(() => {
+    const cityValue = query.get("city");
+    setCity(cityValue);
+  }, [query]);
+
+  useEffect(() => {
+    // Get weather data from weather API
+    if (city) {
+      axios
+        .get(URL)
+        .then(function (response) {
+          setWeatherData(response.data);
+        })
+        .catch(function (error) {
+          console.warn(error);
+        });
+    }
+  }, [URL, city]);
+
   // weatherType value changes which is why it is stored in memo
   const {
     cloudiness,
     currentTemp,
     highTemp,
-    humiditiy,
+    humidity,
     lowTemp,
     weatherType,
     windSpeed,
   } = useMemo(() => {
+    if (!weatherData) return {};
+    // this is where we process data
     return {
-      cloudiness: 100,
-      currentTemp: "76",
-      highTemp: "80",
-      humidity: 100,
-      lowTemp: "80",
-      weatherType: "Cloudy",
-      windSpeed: "10mph",
+      cloudiness: weatherData.clouds.all,
+      currentTemp: fahrenheit(weatherData.main.temp),
+      highTemp: fahrenheit(weatherData.main.temp_max),
+      humidity: weatherData.main.humidity,
+      lowTemp: fahrenheit(weatherData.main.temp_min),
+      weatherType: weatherData.weather[0].main,
+      windSpeed: weatherData.wind.speed,
     };
-  }, []);
+  }, [weatherData]);
   return (
     <main className="App">
-      <header className="Title">Weather App</header>
+      <header>
+        <h3 className="nav">Cities</h3>
+        <div className="cities">
+          <p>
+            <a href="/?city=Paris">Paris</a>
+          </p>
+          <p>
+            <a href="/?city=Tokyo">Tokyo</a>
+          </p>
+        </div>
+      </header>
+      <h1 className="city-name">{city}</h1>
       <WeatherCard
         cloudiness={cloudiness}
         currentTemp={currentTemp}
         highTemp={highTemp}
-        humiditiy={humiditiy}
+        humidity={humidity}
         lowTemp={lowTemp}
         weatherType={weatherType}
         windSpeed={windSpeed}
